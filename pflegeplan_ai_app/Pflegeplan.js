11db95b32263380654980a3b664711cccbb15986
JetlirAl <ninomino428@gmail.com>
2025-08-08 15:39:48 +0200

    Add files via upload

---
+++ pflegeplan_ai_app/Pflegeplan.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+function Pflegeplan() {
+  const [name, setName] = useState('');
+  const [bedarf, setBedarf] = useState('');
+
+  const exportAsPDF = () => {
+    alert('PDF wird exportiert... (Demo-Version)');
+  };
+
+  return (
+    <div>
+      <h3>Pflegeplan erstellen</h3>
+      <input placeholder="Name der Person" value={name} onChange={e => setName(e.target.value)} /><br />
+      <textarea placeholder="Pflegebedarf" value={bedarf} onChange={e => setBedarf(e.target.value)} rows={4} style={{ width: '100%', marginTop: '0.5rem' }} /><br />
+      <button onClick={exportAsPDF} style={{ marginTop: '1rem' }}>Als PDF exportieren</button>
+    </div>
+  );
+}
+
+export default Pflegeplan;
